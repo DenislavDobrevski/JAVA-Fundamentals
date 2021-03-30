9c08fcd513cd9357ca0a66d3b36c82157ee89c50
Denislav Dobrevski <78070754+DenislavDobrevski@users.noreply.github.com>
2021-03-30 19:52:23 +0300

    Add files via upload

---
+++ Arrays_EX/ex2.java
@@ -0,0 +1,23 @@
+package Arrays_EX;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] firstArray = scan.nextLine().split(" ");
+        String[] secondArray = scan.nextLine().split(" ");
+
+        for (int i = 0; i < secondArray.length; i++) {
+            for (int j = 0; j < firstArray.length; j++) {
+                if(secondArray[i].equals(firstArray[j])) {
+                    System.out.print(secondArray[i] + " ");
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
