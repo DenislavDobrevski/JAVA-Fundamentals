76af2c8228e4aa7b2c16577d43688e3711d035cf
Denislav Dobrevski <78070754+DenislavDobrevski@users.noreply.github.com>
2021-06-05 23:55:44 +0300

    Add files via upload

---
+++ RegularExpressions_EX/ex3.java
@@ -0,0 +1,11 @@
+package RegularExpressions_EX;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
